547d420e5c77731be0fb902cb17cc4a813ddabd4
Maria Sedova <mariassedova@mail.ru>
2021-04-19 08:58:04 +0300

    postmanager added

--- src/main/java/ru/netology/manager/PostManager.java
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -1,2 +1,14 @@
-package ru.netology.manager;public class PostManager {
+package ru.netology.manager;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+    private Post[] posts;
+
+    public Post[] search(int ownerId, String domain, String query, boolean ownersOnly, int count, int offset) {
+        return null;
+    }
+    public void delete () {
+
+    }
 }
