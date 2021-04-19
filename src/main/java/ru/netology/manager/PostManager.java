faeb1e61edc4be636c21c34bdc2ce1b18e42243c
Maria Sedova <mariassedova@mail.ru>
2021-04-19 09:01:19 +0300

    postmanager v2 added

--- src/main/java/ru/netology/manager/PostManager.java
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -8,7 +8,7 @@ public class PostManager {
     public Post[] search(int ownerId, String domain, String query, boolean ownersOnly, int count, int offset) {
         return null;
     }
-    public void delete () {
+    public void delete (int ownerId, int postId) {
 
     }
 }
